f8d31965713ddcff183ff147a8e374dfe4cb4776
PEDRO HENRIQUE SOUZA ARCANJO <pedro.si.dev@gmail.com>
2023-12-22 13:57:54 -0300

    ajustes iniciais de themeprovider e correção do 'use client'

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,15 @@
-import { PropsWithChildren } from 'react'
+'use client'
 
+import { PropsWithChildren } from 'react'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/theme'
 import GlobalStyles from '@/styles/global'
 
 export function Providers({ children }: PropsWithChildren) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       {children}
-    </>
+    </ThemeProvider>
   )
 }
