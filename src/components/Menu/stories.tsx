e6566bcad527e2d9e2418cdbc314020899835e75
PEDRO HENRIQUE SOUZA ARCANJO <pedro.si.dev@gmail.com>
2023-12-27 15:17:57 -0300

    Criando componente Menu

---
+++ src/components/Menu/stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, StoryFn } from '@storybook/react'
+import Menu, { MenuProps } from '.'
+
+export default {
+  title: 'Menu',
+  component: Menu
+} as Meta
+
+export const Default: StoryFn<MenuProps> = (args) => <Menu {...args} />
+
+Default.parameters = {
+  layout: 'fullscreen',
+  backgrounds: {
+    default: 'dark'
+  }
+}
